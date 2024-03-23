0e6bc4e3c5db3d0c505bfa7a2252c16122d03d86
RafayDev <imabdulrafayy@gmail.com>
2024-03-23 23:26:16 +0500

    initial comit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+const CONNECTION_URL = 'mongodb+srv://iamabdulrafay:Raf2968%40y@cluster0.zibe59x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
