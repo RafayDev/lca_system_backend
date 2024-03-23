ff18012952c4824b7d09cd95d6d06274bde9f647
RafayDev <imabdulrafayy@gmail.com>
2024-03-24 01:14:40 +0500

    login and resgister api

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+import User from "../models/users.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const register = async (req, res) => {
+    const { name, email, password, role } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ name, email, password: hashedPassword, role });
+        await newUser.save();
+        const data = { user: { id: newUser._id } };
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.status(200).json({ authToken });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const data = { user: { id: user._id } };
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.status(200).json({ authToken });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
+import usersRoutes from './routes/users.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-const CONNECTION_URL = 'mongodb+srv://iamabdulrafay:Raf2968%40y@cluster0.zibe59x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+app.use('/users', usersRoutes);
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
