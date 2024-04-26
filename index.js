7fe9ea3e6fee7dd98b8913b609e3d0d40a79e9d7
RafayDev <imabdulrafayy@gmail.com>
2024-04-26 23:41:02 +0500

    commented teacher and batches

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import usersRoutes from './routes/users.js';
 import coursesRoutes from './routes/courses.js';
-import teachersRoutes from './routes/teachers.js';
-import batchesRoutes from './routes/batches.js';
+// import teachersRoutes from './routes/teachers.js';
+// import batchesRoutes from './routes/batches.js';
 const app = express();
 dotenv.config();
 
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/users', usersRoutes);
 app.use('/courses', coursesRoutes);
-app.use('/teachers', teachersRoutes);
-app.use('/batches', batchesRoutes);
+// app.use('/teachers', teachersRoutes);
+// app.use('/batches', batchesRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
