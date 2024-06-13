6f8dcca495f95f539d3764e56230f41837fe2778
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-06-14 01:24:53 +0500

    fixes into statistics

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -7,67 +7,67 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const getStatistics = async (req, res) => {
-    try {
-        const startOfYear = moment().startOf('year').format('YYYY-MM-DD');
-        const endOfYear = moment().endOf('year').format('YYYY-MM-DD');
-        
-        // Batches Statistics
-        const current_batches_count = await Batch.countDocuments({
-            // count the number of batches of current year based on both startdate and enddate
-            startdate: { $gte: startOfYear },
-        });
+  try {
+    const startOfYear = moment().startOf("year").format("YYYY-MM-DD");
+    const endOfYear = moment().endOf("year").format("YYYY-MM-DD");
 
-        const previous_batches_count = await Batch.countDocuments({
-            // count the number of batches of previous year based on the startdate
-            startdate: { $lt: startOfYear }
-        });
+    // Batches Statistics
+    const current_batches_count = await Batch.countDocuments({
+      startdate: { $gte: startOfYear },
+    });
 
-        const total_batches_count = await Batch.countDocuments(
-            // count the number of overall batches
-        );
+    const previous_batches_count = await Batch.countDocuments({
+      startdate: { $lt: startOfYear },
+    });
 
-        // Students Statistics
-        const total_enrolled_students_count = await Student.countDocuments(
-            // count the total number of students
-        )
+    const total_batches_count = await Batch.countDocuments();
 
-        // Fees Statistics
-        const total_fee_record_result = await Batch.aggregate([{
-            // calculate the total fee record of all batches
-            $group: {
-                _id: null,
-                totalBatchFee: {
-                    $sum: { $toDouble: "$batch_fee" }
-                }
-            }
-        }]);
-        const total_fee_record = total_fee_record_result.length > 0 ? total_fee_record_result[0].totalBatchFee : 0;
+    // Students Statistics
+    const total_enrolled_students_count = await Student.countDocuments();
 
-        const total_fee_recovred_result = await Student.aggregate([{
-            // calculate the total fee recovered from all students
-            $group: {
-                _id: null,
-                totalFeeRecovered: {
-                    $sum: { $toDouble: "$paid_fee" }
-                }
-            }
-        }]);
-        const total_fee_recovred = total_fee_recovred_result.length > 0 ? total_fee_recovred_result[0].totalFeeRecovered : 0;
+    // Fees Statistics
+    const total_fee_record_result = await Student.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalFeeRecord: {
+            $sum: { $toDouble: "$total_fee" },
+          },
+        },
+      },
+    ]);
+    const total_fee_record =
+      total_fee_record_result.length > 0
+        ? total_fee_record_result[0].totalFeeRecord
+        : 0;
 
-        const total_fee_pending = total_fee_record - total_fee_recovred;
+    const total_fee_recovered_result = await Student.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalFeeRecovered: {
+            $sum: { $toDouble: "$paid_fee" },
+          },
+        },
+      },
+    ]);
+    const total_fee_recovered =
+      total_fee_recovered_result.length > 0
+        ? total_fee_recovered_result[0].totalFeeRecovered
+        : 0;
 
-        res.status(200).json({
-            current_batches_count,
-            previous_batches_count,
-            total_batches_count,
-            total_enrolled_students_count,
-            total_fee_record,
-            total_fee_recovred,
-            total_fee_pending
-        });
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+    const total_fee_pending = total_fee_record - total_fee_recovered;
 
+    res.status(200).json({
+      current_batches_count,
+      previous_batches_count,
+      total_batches_count,
+      total_enrolled_students_count,
+      total_fee_record,
+      total_fee_recovered,
+      total_fee_pending,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
