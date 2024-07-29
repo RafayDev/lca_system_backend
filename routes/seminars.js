e79e9a2daa49ac0fbf21bb474ec6ea9ca82600a7
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-07-29 12:20:53 +0500

    remove auth from get today seminar

--- routes/seminars.js
+++ routes/seminars.js
@@ -10,7 +10,7 @@ router.get('/:id',auth,getSeminar)
 router.post('/add',auth,addSeminar);
 router.post('/update/:id',auth,updateSeminar);
 router.delete('/delete/:id',auth,deleteSeminar);
-router.get('/seminar/today',auth,getTodaySeminars);
+router.get('/seminar/today',getTodaySeminars);
 router.get('/seminar/getSeminarAttendees',getSeminarsWithAttendeeCounts);
 
 export default router
