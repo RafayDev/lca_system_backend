a835aa0eb79ebc0c5d29739254d588914dccc501
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-10-21 18:27:35 +0500

    fixes into latest degree

--- controllers/students.js
+++ controllers/students.js
@@ -203,7 +203,7 @@ export const updateStudent = async (req, res) => {
     const latestDegreeImageFileName = `latest_degree_${id}${latestDegreeImageFileExt}`;
     await uploadFile(latestDegreeImageFile, latestDegreeImageFileName, `${filesStoragePath}/students/latest_degree`);
     const latestDegreeImageWebpFileName = `latest_degree_${id}.jpeg`;
-    await compressImage(`${filesStoragePath}/students/latest_degree/${cnicBackImageFileName}`, `${filesStoragePath}/students/cnic_images/${cnicBackImageWebpFileName}`, 50);
+    await compressImage(`${filesStoragePath}/students/latest_degree/${latestDegreeImageFileName}`, `${filesStoragePath}/students/latest_degree/${cnicBackImageWebpFileName}`, 50);
     const latest_degree_imagePath = `${filesStorageUrl}/files/students/latest_degree/${latestDegreeImageWebpFileName}`
 
     // Update the student record
@@ -332,7 +332,7 @@ export const updateStudentinfo = async (req, res) => {
     const latestDegreeImageFileName = `latest_degree_${id}${latestDegreeImageFileExt}`;
     await uploadFile(latestDegreeImageFile, latestDegreeImageFileName, `${filesStoragePath}/students/latest_degree`);
     const latestDegreeImageWebpFileName = `latest_degree_${id}.jpeg`;
-    await compressImage(`${filesStoragePath}/students/latest_degree/${cnicBackImageFileName}`, `${filesStoragePath}/students/latest_degree/${cnicBackImageWebpFileName}`, 50);
+    await compressImage(`${filesStoragePath}/students/latest_degree/${latestDegreeImageFileName}`, `${filesStoragePath}/students/latest_degree/${cnicBackImageWebpFileName}`, 50);
     const latest_degree_imagePath = `${filesStorageUrl}/files/students/latest_degree/${latestDegreeImageWebpFileName}`
 
     // Update the student record
